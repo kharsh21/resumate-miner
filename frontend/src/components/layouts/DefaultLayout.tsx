328edafba32865e2de453e044e9e5b6029e41212
Harsh Khandelwal <kharsh21@iitk.ac.in>
2024-08-28 15:03:14 +0530

    imports

--- frontend/src/components/layouts/DefaultLayout.tsx
+++ frontend/src/components/layouts/DefaultLayout.tsx
@@ -29,6 +29,9 @@
 // };
 
 // export default DefaultLayout;
+import React, { ReactNode } from "react";
+import Footer from "@/components/modules/Footer";
+import Header from "@/components/modules/Header";
 import Head from 'next/head';
 
 interface DefaultLayoutProps {
